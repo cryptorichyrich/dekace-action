ebbfea3d9e7f987602bc0b9f10ed462f8305299b
Agustinus Biotamalo Lumbantoruan <92082038+cryptorichyrich@users.noreply.github.com>
2025-01-19 01:58:20 +0700

    Update live.js

--- live.js
+++ live.js
@@ -68,7 +68,10 @@ async function deleteLiveJsonFile() {
 }
 
 async function main() {
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ 
+        headless: "new", 
+        args: ['--no-sandbox'] 
+    });
     const page = await browser.newPage();
 
     try {
